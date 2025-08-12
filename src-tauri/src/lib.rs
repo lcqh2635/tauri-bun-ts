3680bdc1b2bb3536192f7f1c5565de56246f9e7d
龙茶清欢 <2320391937@qq.com>
2025-08-12 18:38:05 +0800

    新建项目

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,14 +16,31 @@ pub fn run() {
         // 使用设置挂钩执行与设置相关的任务
         // 在主循环之前运行，因此尚未创建任何窗口
         .setup(|app| {
+
+            // 创建一个自动启动插件，在系统启动时自动启动您的应用程序
+            // 具体使用请参考 https://v2.tauri.org.cn/plugin/autostart/
             #[cfg(desktop)]
-            // create_system_tray(app);
+            app.handle()
+                .plugin(tauri_plugin_autostart::init(
+                    /* 要传递给应用的任意数量的参数 */
+                    tauri_plugin_autostart::MacosLauncher::LaunchAgent, Some(vec!["--flag1", "--flag2"])
+                ))
+                .expect("TODO: panic message");
+
+            // 添加二维码扫描插件，允许您的移动应用程序使用相机扫描二维码、EAN-13 和其他类型的条形码。
+            // 具体使用请参考 https://v2.tauri.org.cn/plugin/barcode-scanner/
+            #[cfg(mobile)]
+            app.handle()
+                .plugin(tauri_plugin_barcode_scanner::init())
+                .expect("TODO: panic message");
+
 
             // 钩子期望一个 Ok 结果
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
